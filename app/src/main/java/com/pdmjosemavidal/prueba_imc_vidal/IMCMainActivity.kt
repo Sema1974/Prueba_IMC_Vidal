fb55645df5a65c13321bd1411561653bd5da1ba7
Sema <vidalbuzon@gmail.com>
2024-11-11 22:35:21 +0100

    Commit Kasa 1

---
+++ app/src/main/java/com/pdmjosemavidal/prueba_imc_vidal/IMCMainActivity.kt
@@ -0,0 +1,128 @@
+package com.pdmjosemavidal.prueba_imc_vidal
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.icu.text.DecimalFormat
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.slider.Slider
+
+
+class IMCMainActivity : AppCompatActivity() {
+    private lateinit var btnCalcular:AppCompatButton
+    private lateinit var viewMale:CardView
+    private lateinit var viewFemale:CardView
+    private lateinit var tvHeight:TextView
+    private lateinit var rsHeight:Slider
+    private lateinit var viewAge:TextView
+    private lateinit var btnSubtractAge:FloatingActionButton
+    private lateinit var btnAddAge:FloatingActionButton
+    private lateinit var  viewWeight:TextView
+    private lateinit var btnSubtractWeight:FloatingActionButton
+    private lateinit var btnAddWeight:FloatingActionButton
+    private  var isMaleSelected = true
+    private  var ageStart = 20
+    private var weightStart = 60
+    private var heightStart = 120
+    private var resultado = 0.0
+
+    private fun initUI() {
+        setGenderColor()
+        setAge(0)
+        setWeight(0)
+    }
+    private fun initListeners() {
+        viewMale.setOnClickListener{
+            isMaleSelected = true
+            setGenderColor()
+        }
+        viewFemale.setOnClickListener{
+            isMaleSelected = false
+            setGenderColor()
+        }
+        rsHeight.addOnChangeListener{_, value,_-> tvHeight.text=DecimalFormat("#.##").format(value) + " cm"}
+        btnSubtractAge.setOnClickListener(){
+            setAge(-1)
+        }
+        btnAddAge.setOnClickListener(){
+            setAge(1)
+        }
+        btnSubtractWeight.setOnClickListener(){
+            setWeight(-1)
+        }
+        btnAddWeight.setOnClickListener(){
+            setWeight(1)
+        }
+        btnCalcular.setOnClickListener(){
+            val intentGA = Intent(this, IMCResulActivity::class.java)
+            intentGA.putExtra("EXTRA_NAME", navigate2result(calculateIMC()))
+            startActivity(intentGA)
+
+        }
+    }
+    private fun getBackgroundColor(isComponentSelected: Boolean): Int {
+
+        val colorReference = if(isComponentSelected){
+            R.color.bg_comp_sel
+        }else{
+            R.color.bg_comp
+        }
+        return ContextCompat.getColor(this, colorReference)
+    }
+
+    private fun initComponents() {
+        viewMale=findViewById(R.id.viewMale)
+        viewFemale=findViewById(R.id.viewFemale)
+        rsHeight=findViewById(R.id.rsHeight)
+        viewAge=findViewById(R.id.viewAge)
+        btnSubtractAge=findViewById(R.id.btnSubtractAge)
+        btnAddAge=findViewById(R.id.btnAddAge)
+        viewWeight=findViewById(R.id.viewWeight)
+        btnSubtractWeight=findViewById(R.id.btnSubtractWeight)
+        btnAddWeight=findViewById(R.id.btnAddWeight)
+        btnCalcular=findViewById(R.id.botonCalcular)
+
+    }
+    private fun calculateIMC(): Double{
+        val alturaMetro : Double = tvHeight.text.toString().replace(" cm", "").toDouble()/100
+        resultado = (weightStart.toDouble()/(alturaMetro*alturaMetro))
+        return resultado
+    }
+    private fun navigate2result(imc : Double):String{
+        return imc.toString()
+    }
+
+    private fun setGenderColor() {
+        viewMale.setCardBackgroundColor(getBackgroundColor(isMaleSelected))
+        viewFemale.setCardBackgroundColor(getBackgroundColor(!isMaleSelected))
+    }
+    private fun setAge(nume: Int){
+        ageStart = ageStart+nume
+        viewAge.text=ageStart.toString()
+    }
+    private fun setWeight(nume: Int){
+        weightStart = weightStart+nume
+        viewWeight.text=weightStart.toString()
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_imcmain)
+        initComponents()
+        initListeners()
+        initUI()
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/pdmjosemavidal/prueba_imc_vidal/IMCResulActivity.kt
@@ -0,0 +1,77 @@
+package com.pdmjosemavidal.prueba_imc_vidal
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.appcompat.widget.AppCompatButton
+
+class IMCResulActivity : AppCompatActivity() {
+    private lateinit var btnReCal: AppCompatButton
+    private lateinit var tvReFi: TextView
+    private lateinit var tvRes: TextView
+    private lateinit var tvComentario: TextView
+
+    private fun initListeners(){
+        btnReCal.setOnClickListener(){
+            finish()
+        }
+    }
+    private fun mostrarResul(valorRel: Double){
+        when(valorRel) {
+            in 0.00..18.50 -> {
+                tvReFi.text = getString(R.string.muydelgado)
+                tvReFi.setTextColor(ContextCompat.getColor(this, R.color.red))
+                tvComentario.text = getString(R.string.comentario_delgado)
+
+            }
+            in 18.51..24.99 -> {
+                tvReFi.text = getString(R.string.estupendo)
+                tvReFi.setTextColor(ContextCompat.getColor(this, R.color.green))
+                tvComentario.text = getString(R.string.comentario_ideal)
+            }
+            in 25.00..29.99-> {
+                tvReFi.text = getString(R.string.sobrepeso)
+                tvReFi.setTextColor(ContextCompat.getColor(this, R.color.mage))
+                tvComentario.text = getString(R.string.comentario_sobrepeso)
+            }
+            else -> {
+                tvReFi.text = getString(R.string.muchosobrepeso)
+                tvReFi.setTextColor(ContextCompat.getColor(this, R.color.orqui))
+                tvComentario.text = getString(R.string.comentario_muchosobrepeso)
+            }
+        }
+    }
+
+    private fun initComponents() {
+        btnReCal = findViewById(R.id.botonReCalcular)
+        tvReFi = findViewById(R.id.resultadofinal)
+        tvRes = findViewById(R.id.numefinal)
+        tvComentario = findViewById(R.id.sentenciafinal)
+
+    }
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_imcresul)
+
+        val valorRel:Double = intent.extras?.getDouble("EXTRA_NAME") ?: -1.0
+
+        initComponents()
+        initListeners()
+        mostrarResul(valorRel)
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
